989172a8142d1de450d986912acf102f1d7944cc
Sonder82 <Himrs82@gmail.com>
2023-01-18 21:46:45 +0500

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -0,0 +1,90 @@
+package ru.job4j.dreamjob.model;
+
+
+import java.util.Objects;
+
+/**
+ * Класс содержит модель, описывающую вакансию.
+ */
+public class Vacancy {
+    /**
+     * поле id
+     */
+    private int id;
+    /**
+     * поле название вакансии
+     */
+    private String title;
+    /**
+     * поле описание вакансии
+     */
+    private String description;
+    /**
+     * поле дата создания вакансии
+     */
+    private String creationDate;
+
+    public Vacancy(int id, String title, String description, String creationDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.creationDate = creationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    /**
+     * Можно исключить поля title, description, created.
+     * Эти поля могут иметь одинаковые значение у разных объектов.
+     * @param o объект
+     * @return boolean
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vacancy vacancy = (Vacancy) o;
+        return id == vacancy.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+
+
---
+++ src/main/java/ru/job4j/dreamjob/repository/MemoryVacancyRepository.java
@@ -0,0 +1,65 @@
+package ru.job4j.dreamjob.repository;
+
+import ru.job4j.dreamjob.model.Vacancy;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class MemoryVacancyRepository implements VacancyRepository {
+
+    private static final MemoryVacancyRepository INSTANCE = new MemoryVacancyRepository();
+
+    private int nextId = 1;
+
+    private final Map<Integer, Vacancy> vacancies = new HashMap<>();
+
+    public MemoryVacancyRepository() {
+        String localDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+        save(new Vacancy(0, "Intern Java Developer",
+                "We are looking for a Intern Java Developer", localDateTime));
+        save(new Vacancy(0, "Junior Java Developer",
+                "We are looking for a Junior Java Developer", localDateTime));
+        save(new Vacancy(0, "Junior+ Java Developer",
+                "We are looking for a Junior+ Java Developer", localDateTime));
+        save(new Vacancy(0, "Middle Java Developer",
+                "We are looking for Middle Java Developer", localDateTime));
+        save(new Vacancy(0, "Middle+ Java Developer",
+                "We are looking for a Middle+ Java Developer", localDateTime));
+        save(new Vacancy(0, "Senior Java Developer",
+                "We are looking for a Senior Java Developer", localDateTime));
+    }
+
+    public static MemoryVacancyRepository getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public Vacancy save(Vacancy vacancy) {
+        vacancy.setId(nextId++);
+        vacancies.put(vacancy.getId(), vacancy);
+        return vacancy;
+    }
+
+    @Override
+    public boolean deleteById(int id) {
+        return vacancies.remove(id) != null;
+    }
+
+    @Override
+    public boolean update(Vacancy vacancy) {
+        return vacancies.computeIfPresent(
+                vacancy.getId(), (id, oldVacancy) -> new Vacancy(
+                        id, vacancy.getTitle(), vacancy.getDescription(), vacancy.getCreationDate())) != null;
+    }
+
+    @Override
+    public Optional<Vacancy> findById(int id) {
+        return Optional.ofNullable(vacancies.get(id));
+    }
+
+    @Override
+    public Collection<Vacancy> findAll() {
+        return vacancies.values();
+    }
+}
